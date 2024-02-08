e1b95a381c09d9a2484407d8199332bb2a5259ec
ryujinzz <kdo2240k@naver.com>
2024-02-08 14:29:27 +0900

    [초기설정] tailwindcss 추가

--- react-homework/src/App.jsx
+++ react-homework/src/App.jsx
@@ -1,7 +1,11 @@
 import { useState } from 'react';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="bg-yellow-500 text-center">안녕 클레오파트라</div>
+    </>
+  );
 }
 
 export default App;
